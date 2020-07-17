7fda65c5481f84bf9e07e478edbdf48c3631eff7
brookwalls <bwalls@blackpearltechnology.com>
2020-07-17 08:06:51 -0500

    programs needed

---
+++ ex3.py
@@ -0,0 +1,39 @@
+# print out "I will not count my chickens:"
+print "I will not count my chickens:"
+
+# print out "Hens:" and computes 30 / 6 then 25 + 5
+print "Hens:", 25 + 30 / 6
+# print out "Roosters:" and computes 25 * 3 then 75 % 4 then 100 - 3
+print "Roosters:", 100 - 25 * 3 % 4
+
+# print out "Now I will count the eggs:" and computes 4 % 2 then 1 / 4 then add and subtracts the rest together
+print "\nNow I will count the eggs:", 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+# print out "Is it true that 3 + 2 < 5 - 7"
+print "\nIs it true that 3 + 2 < 5 - 7?"
+
+# computes 5 < -2 prints out false 
+print 3 + 2 < 5 - 7
+
+# print out "What is 3 + 2?" and computes 3 + 2 to 5
+print "What is 3 + 2?", 3 + 2
+
+# print out "What is 5 - 7?" and computes 5 - 7 to -2
+print "What is 5 - 7?", 5 - 7
+
+# print out "Oh, that's why it's False."
+print "Oh, that's why it's False."
+
+# print out "How about some more."
+print "\nHow about some more."
+
+print 208839 * 47493927 / 578 
+print 30 * 39 * 29 * 40 * 10
+
+# print out "Is it greater?" and computes 5 > -2 to true
+print "Is it greater?", 5 > -2
+# print out "Is it greater or equal?" and computes 5 >= -2 to true
+print "Is it greater or equal?", 5 >= -2
+# print out "Is it less or equal?" and computes 5 <= -2 to false
+print "Is it less or equal?", 5 <= -2
+
