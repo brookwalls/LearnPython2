8a2c808c9a670e55fd0071be4a7146e0aa8d610f
brookwalls <bwalls@blackpearltechnology.com>
2020-07-17 14:44:17 -0500

    Exercise 13

--- ex13.py
+++ ex13.py
@@ -6,5 +6,5 @@ print "The scrpt is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
-
+height = raw_input("How tall are you? ")
 
